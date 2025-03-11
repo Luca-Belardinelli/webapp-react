ed33024108877cfa72765979bb84f6a25ae54001
Luca Belardinelli <belardinelliluca@gmail.com>
2025-03-11 16:58:54 +0100

    Creation Header Layout pages

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+// IMPORTIAMO IL LINK DEL MODULO REACT-ROUTER
+
+import { Link } from "react-router-dom"
+
+const Header = () => {
+
+    return (
+        <nav>
+            <div>
+                <Link>MOVIES</Link>
+            </div>
+        </nav>
+    )
+
+}
+
+export default Header
