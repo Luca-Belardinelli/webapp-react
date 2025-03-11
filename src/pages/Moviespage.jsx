2c8c9a7ff1554de60ac185bcc5ce26ecf4b9b9ea
Luca Belardinelli <belardinelliluca@gmail.com>
2025-03-11 17:59:34 +0100

    fix moviepage

--- src/pages/Moviespage.jsx
+++ src/pages/Moviespage.jsx
@@ -1,13 +1,26 @@
+// IMPORTIAMO IL LINK DEL MODULO REACT-ROUTER
 
+import { Link } from "react-router-dom"
 
 const Moviespage = () => {
     return (
-        <div>
-            <h2>Titolo FIlm</h2>
-            <span>Autore</span>
-            <p>ciao ciao ciao ciao ciao ciao</p>
+        <>
+            <div>
+                <h2>Titolo FIlm</h2>
+                <span>Autore</span>
+                <p>ciao ciao ciao ciao ciao ciao</p>
+            </div>
 
-        </div>
+            <div>
+                <h3>community reviews</h3>
+                {/* reviews */}
+                <p>reviews</p>
+            </div>
+
+            <div>
+                <Link to="/">Back to home</Link>
+            </div>
+        </>
     )
 }
 
