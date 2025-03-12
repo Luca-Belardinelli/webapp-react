af7c83af16ecda3e3dbbbc192a9fb449c48d1b44
Luca Belardinelli <belardinelliluca@gmail.com>
2025-03-12 17:08:11 +0100

    props on movielist

--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -1,29 +1,20 @@
 import { Link } from "react-router-dom"
 
-export default function MovieListCard() {
+export default function MovieListCard({ movieProp }) {
 
-
+    //DESTRUTTURIAMO OGGETTO PROPS
+    const { id, title, director, genre, release_year, abstract, image } = movieProp;
     return (
-
-
         <div >
-            <h5 >Tre Uomini e una gamba</h5>
-
-
-            <address><i>By nome autore</i></address>
-
+            {image}
             <div >
-                <p>
-                </p>
-
+                <h5 >{title}</h5>
+                <address><i>{director}</i></address>
+                <p>{abstract}</p>
+                <p>{genre} , {release_year}</p>
                 <Link to="movies/2">Dettagli</Link>
             </div>
-
-
-
-
         </div>
-
 
     );
 }
